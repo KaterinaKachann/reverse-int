cac46cdd59d4e591cd0a5a6deda6227d7e4bc7d3
Katerina Kachan <eka.kachan@gmail.com>
2020-10-25 16:05:38 +0300

    feat: add solution reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function reverse (n) {
-  
+module.exports = 
+function reverse (n) {
+
+    n = Math.abs(n);
+    let string = n.toString();
+    let result = '';
+
+    for (let i = 0; i < string.length; i++) {
+        result = result + string[string.length - i-1];
+    }
+
+    return parseInt(result);
 }
+
+// console.log(reverse(-3768));
+
+
